31e9c13e00b17e0e9fe367401d574f15251fbbc5
skiwi <frank_skiwi@hotmail.com>
2014-10-28 14:00:55 +0100

    A notification will be shown in the client if an exception has occured in any thread. The detailed view still needs to be implemented.

--- cardshifter-fx/src/main/java/com/cardshifter/newfx/ClientController.java
+++ cardshifter-fx/src/main/java/com/cardshifter/newfx/ClientController.java
@@ -11,7 +11,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import org.controlsfx.control.action.Action;
 
 import com.cardshifter.newfx.base.Views;
 
@@ -29,6 +33,17 @@ public final class ClientController extends ContentController implements Initial
 	@Override
 	public void initialize(final URL url, final ResourceBundle resourceBundle) {
 		super.initialize(url, resourceBundle);
+		
+		Thread.setDefaultUncaughtExceptionHandler((thread, ex) -> {
+			useNotificationPane(notificationPane -> {
+				Action action = new Action("Details", actionEvent -> {
+//					//TODO showDialog
+				});
+				notificationPane.show(ex.getClass().getSimpleName() + ": " + ex.getMessage() + " on " + thread, new ImageView(new Image(getClass().getResourceAsStream("dialog-error.png"))), action);
+			});
+			ex.printStackTrace();
+		});
+		
 		buildLabel.setText("Build: " + getClass().getPackage().getImplementationVersion());
 		setContent(Views.load(getClass().getResource("Homepage.fxml"), new HomepageController(stage, this)));
 	}
