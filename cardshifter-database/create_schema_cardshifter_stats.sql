7dd1df5df8df2516b35fc6092b483ae2530a84a0
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2014-10-28 20:34:43 -0400

    Updated cards list. Also updated SQL cardshifter_stats design.

--- cardshifter-database/create_schema_cardshifter_stats.sql
+++ cardshifter-database/create_schema_cardshifter_stats.sql
@@ -6,27 +6,27 @@ SET SEARCH_PATH TO cardshifter_stats;
 
 CREATE TABLE player 
 (
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY UNIQUE,
 	name TEXT NOT NULL,
 	password TEXT NOT NULL, -- this will be made secure once implemented
 	email TEXT,
 	website TEXT NULL,
 	about TEXT NULL,
-	create_date TIMESTAMP NOT NULL DEFAULT NOW(),
-	delete_date TIMESTAMP NULL DEFAULT NULL,
-	last_seen_date TIMESTAMP NULL DEFAULT NULL
+	create_time TIMESTAMP NOT NULL DEFAULT NOW(),
+	delete_time TIMESTAMP NULL DEFAULT NULL,
+	last_seen_time TIMESTAMP NULL DEFAULT NULL
 );
 CREATE UNIQUE INDEX player_name_idx ON player(name);
 CREATE TABLE mod 
 (
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY UNIQUE,
 	name TEXT NOT NULL,
 	version INT NOT NULL, -- incremented when modified
 	description TEXT NULL,
 	owner_player_id INT NOT NULL,
 		FOREIGN KEY (owner_player_id) REFERENCES player(id),
-	create_date TIMESTAMP NOT NULL DEFAULT NOW(),
-	delete_date TIMESTAMP NULL DEFAULT NULL
+	create_time TIMESTAMP NOT NULL DEFAULT NOW(),
+	delete_time TIMESTAMP NULL DEFAULT NULL
 );
 CREATE UNIQUE INDEX mod_name_idx ON mod(name);
 CREATE TABLE player_mod 
@@ -38,18 +38,20 @@ CREATE TABLE player_mod
 );
 CREATE TABLE card_type
 (
-	name TEXT PRIMARY KEY
+	name TEXT PRIMARY KEY UNIQUE
 );
 INSERT INTO card_type (name) VALUES
-	('B0T'),
+	('Mech'),
+	('Hume'),
 	('Bio'),
-	('Enhancement');
+	('Upgrade'),
+	('Spell');
 CREATE TABLE card 
 (
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY UNIQUE,
 	version INT NOT NULL, -- incremented when modified
 	name TEXT NULL,
-	description TEXT NULL,
+	flavor_text TEXT NULL,
 	effect_description TEXT NULL,
 	type TEXT NULL,
 		FOREIGN KEY (type) REFERENCES card_type(name),
@@ -60,54 +62,45 @@ CREATE TABLE card
 	scrap_value INT NULL,
 	sickness INT NULL,
 	attack_available INT NULL,
-	create_date TIMESTAMP NOT NULL DEFAULT NOW(),
-	delete_date TIMESTAMP NULL DEFAULT NULL
+	create_time TIMESTAMP NOT NULL DEFAULT NOW(),
+	delete_time TIMESTAMP NULL DEFAULT NULL
 );
 CREATE INDEX card_name_idx ON card(name);
 CREATE TABLE deck
 (
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY UNIQUE,
 	player_id INT NOT NULL,
 		FOREIGN KEY (player_id) REFERENCES player(id),
 	mod_id INT NOT NULL,
-		FOREIGN KEY (mod_id) REFERENCES mod(id)
+		FOREIGN KEY (mod_id) REFERENCES mod(id),
+	name TEXT NOT NULL
 );
 CREATE TABLE deck_card
 (
 	deck_id INT,
 		FOREIGN KEY (deck_id) REFERENCES deck(id),
-	version INT NOT NULL, -- incremented when modified
+	version INT NOT NULL, 	-- incremented when modified
 	card_id INT NOT NULL,
 		FOREIGN KEY (card_id) REFERENCES card(id),
 	card_quantity INT NOT NULL
 );
-CREATE TABLE game_master
+CREATE TABLE game
 (
-	game_id SERIAL PRIMARY KEY,
-	player1 INT,
-		FOREIGN KEY (player1) REFERENCES player(id),
-	player2 INT,
-		FOREIGN KEY (player2) REFERENCES player(id),
-	start_time TIMESTAMP NOT NULL DEFAULT NOW(),
-	end_time TIMESTAMP NULL DEFAULT NULL
+	id SERIAL UNIQUE,
+	mod_id INT NOT NULL,
+		FOREIGN KEY (mod_id) REFERENCES mod(id),
+	start_time TIMESTAMP NOT NULL
 );
-CREATE TABLE game_actions
+CREATE TABLE game_player
 (
 	game_id INT NOT NULL,
-		FOREIGN KEY (game_id) REFERENCES game_master(game_id),
-	turn INT NOT NULL,
-	action_player INT NOT NULL,
-		FOREIGN KEY (action_player) REFERENCES player(id),
-	action_card INT NULL,
-		FOREIGN KEY (action_card) REFERENCES card(id),
-	action_attack INT NULL,
-	target_player INT NULL,
-		FOREIGN KEY (target_player) REFERENCES player(id),
-	target_card INT NULL,
-		FOREIGN KEY (target_card) REFERENCES card(id),
-	action_time TIMESTAMP NOT NULL
+		FOREIGN KEY (game_id) REFERENCES game(id),
+	player_id INT NOT NULL,
+		FOREIGN KEY (player_id) REFERENCES player(id),
+	player_deck INT NOT NULL,
+		FOREIGN KEY (player_deck) REFERENCES deck(id),
+	game_rank INT NOT NULL
 );
-
 COMMIT;
 
 SELECT
