16d90abb02f3b28ba3954260c5f860e7ec189275
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2014-10-29 20:06:43 -0400

    Updated excel and sql files.

--- cardshifter-database/cardshifter-0.4-player-list.sql
+++ cardshifter-database/cardshifter-0.4-player-list.sql
@@ -1,11 +1,25 @@
 ﻿-- SQL insert query
-START TRANSACTION; SET SEARCH_PATH TO cardshifter_stats; DELETE FROM player;
-INSERT INTO player(name,password,email,website,about,delete_date,last_seen_date) VALUES
+START TRANSACTION; SET SEARCH_PATH TO cardshifter_stats; 
+DROP TABLE IF EXISTS player CASCADE;
+CREATE TABLE player 
+(
+	id SERIAL PRIMARY KEY UNIQUE,
+	name TEXT NOT NULL,
+	password TEXT NOT NULL, -- this will be made secure once implemented
+	email TEXT,
+	website TEXT NULL,
+	about TEXT NULL,
+	create_time TIMESTAMP NOT NULL DEFAULT NOW(),
+	delete_time TIMESTAMP NULL DEFAULT NULL,
+	last_seen_time TIMESTAMP NULL DEFAULT NULL
+);
+INSERT INTO player(name,password,email,website,about,delete_time,last_seen_time) VALUES
 ('Zomis','*****',null,null,null,null,null),
 ('bazola','*****',null,null,null,null,null),
 ('skiwi','*****',null,null,null,null,null),
 ('Phrancis','*****',null,null,null,null,null),
 ('GreyFox','*****',null,null,null,null,null),
-('Jay1148','*****',null,null,null,null,null);
+('Jay1148','*****',null,null,null,null,null); 
 COMMIT;
+
 SELECT * FROM player;
